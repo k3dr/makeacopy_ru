785d2b4c54465e06092664c96350ad5fc05b3a6d
christian <christian.kierdorf@schliweb.de>
2025-08-27 17:19:49 +0200

    Version 1.1.3

--- external/opencv_pinned_jni/opencv_jni.hpp
+++
@@ -1,6 +0,0 @@
-// egdels did ;-)
-#include "/tmp/opencv-build_arm64-v8a/modules/java_bindings_generator/gen/cpp/core.inl.hpp"
-#include "/tmp/opencv-build_arm64-v8a/modules/java_bindings_generator/gen/cpp/imgproc.inl.hpp"
-#include "/tmp/opencv-build_arm64-v8a/modules/java_bindings_generator/gen/cpp/video.inl.hpp"
-#include "/tmp/opencv-build_arm64-v8a/modules/java_bindings_generator/gen/cpp/imgcodecs.inl.hpp"
-#include "/tmp/opencv-build_arm64-v8a/modules/java_bindings_generator/gen/cpp/videoio.inl.hpp"
